0888eb490019f34dd954a9f88ec21fa02c6fbca9
Vitalie Miron <vm@insomniacdesign.com>
2021-04-02 14:03:10 +0300

    init

---
+++ .history/src/components/movie-list/index_20210402135201.js
@@ -0,0 +1,37 @@
+import { setState } from "expect";
+import React, { useState } from "react";
+import "./index.css";
+
+function MovieList() {
+
+  const [year, setYear] = useState('');
+  const [movies, setMovies] = useState([]);
+
+  const getMovies = (year = 2015) => {
+    fetch(`https://jsonmock.hackerrank.com/api/movies?Year=${year}`)
+      .then(res => res.json())
+      .then(
+        (result) => {
+          setMovies(result.data)
+        }
+      )
+  }
+
+  console.log(movies)
+  return (
+    <div className="layout-column align-items-center mt-50">
+      <section className="layout-row align-items-center justify-content-center">
+        <input type="number" className="large" placeholder="Enter Year eg 2015" data-testid="app-input" value={year} onChange={(e) => { setYear(e.target.value) }} />
+        <button className="" data-testid="submit-button" onClick={() => getMovies(year)}>Search</button>
+      </section>
+
+      <ul className="mt-50 styled" data-testid="movieList">
+        {movies.map(movie => (<li>{movie.name}</li>))}
+      </ul>
+
+      <div className="mt-50 slide-up-fade-in" data-testid="no-result"></div>
+    </div>
+  );
+}
+
+export default MovieList
